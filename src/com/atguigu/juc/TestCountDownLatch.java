0a46bb27f560f61ecb890db38eb8e03d775f8dea
wojiaochadi <264835229@qq.com>
2019-12-30 13:06:48 +0800

    使用闭锁，才能正确计算所有线程执行完消耗的时间 添加闭锁代码

--- src/com/atguigu/juc/TestCountDownLatch.java
+++ src/com/atguigu/juc/TestCountDownLatch.java
@@ -9,12 +9,22 @@ public class TestCountDownLatch {
     * */
     public static void main(String[] args) {
 
+        final CountDownLatch latch = new CountDownLatch(10);
+
         long start = System.currentTimeMillis();
-        LatchDemo latchDemo = new LatchDemo();
+        LatchDemo latchDemo = new LatchDemo(latch);
 
         for (int i = 0; i < 10; i++) {
             new Thread(latchDemo).start();
         }
+
+        //只有latch数减为0的时候，此能执行完成此条语句，不然无限等待下去
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         long end = System.currentTimeMillis();
 
         System.out.println("所有线程执行完成所需要时间：" + (end - start));
@@ -23,12 +33,24 @@ public class TestCountDownLatch {
 
 class LatchDemo implements Runnable {
 
+    private CountDownLatch latch;
+
+    public LatchDemo(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
     @Override
     public void run() {
-        for (int i = 0; i < 500; i++) {
-            if (i % 2 == 0) {
-                System.out.println(i);
+
+        try {
+            for (int i = 0; i < 500; i++) {
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
             }
+        }finally {
+            //必须要执行
+            latch.countDown();
         }
     }
 }
