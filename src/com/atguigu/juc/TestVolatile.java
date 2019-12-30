5a52f323387fcf553960f5fb4b26c8ca232cb2da
wojiaochadi <264835229@qq.com>
2019-12-30 11:00:09 +0800

    内存可见性： main线程早于ThreadDemo线程获取了 flag=false的数据，并且while(true)效率太高导致没有机会重新去获取flag的新数据
    添加synchronized，锁住对象，每次都去刷新数据

--- src/com/atguigu/juc/TestVolatile.java
+++ src/com/atguigu/juc/TestVolatile.java
@@ -12,13 +12,16 @@ public class TestVolatile {
 
         //while (true) 执行效率非常高，main没有机会从主存中再次去获取数据，所以在else里面进行其他操作（或者debugger模式下），就有机会去主存里面获取数据
         while (true) {
-            if(td.isFlag()){
-                //按理说应该能够打印这行，但实际不行，因为内存可见性的问题，main线程和ThreadDemo是两个线程，
-                System.out.println("------------");
-                break;
-            }else{
-                //如果else里面进行操作，则降低了while(true)的效率，让main线程有机会去主存获取数据
-                //System.out.println("+++");
+            //将td锁住，每次使用td的时候都去刷新一下数据
+            synchronized (td){
+                if(td.isFlag()){
+                    //按理说应该能够打印这行，但实际不行，因为内存可见性的问题，main线程和ThreadDemo是两个线程，
+                    System.out.println("------------");
+                    break;
+                }else{
+                    //如果else里面进行操作，则降低了while(true)的效率，让main线程有机会去主存获取数据
+                    //System.out.println("+++");
+                }
             }
         }
     }
