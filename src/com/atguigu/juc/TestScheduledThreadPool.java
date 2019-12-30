755e1d4c8a0433d5202dc4abf7f1954d70561979
wojiaochadi <264835229@qq.com>
2019-12-30 18:03:03 +0800

    线程池线程调度 线程池的体系结构：   java.util.concurrent.Executor：负责线程的使用和调度的根接口       |--ExecutorService：子接口  线程池的主要接口           |--ThreadPoolExecutor 实现类           |--ScheduledExecutorService 子接口：负责线程的调度               |--ScheduledThreadPoolExecutor ：继承了ThreadPoolExecutor，实现了ScheduledExecutorService接口

---
+++ src/com/atguigu/juc/TestScheduledThreadPool.java
@@ -0,0 +1,40 @@
+package com.atguigu.juc;
+
+import java.util.Random;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+/*
+* 线程调度
+* */
+public class TestScheduledThreadPool {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ScheduledExecutorService pool = Executors.newScheduledThreadPool(5);
+
+        for (int i = 0; i < 10; i++) {
+            //ScheduledFuture<Integer> future = pool.schedule(new Callable<Integer>() {
+            //    @Override
+            //    public Integer call() throws Exception {
+            //        int num = new Random().nextInt(100);
+            //        System.out.println(Thread.currentThread().getName() + " : " + num);
+            //        return num;
+            //    }
+            //}, 2, TimeUnit.SECONDS);
+
+            //线程池写法 Callable lambda表达式写法
+            ScheduledFuture<Integer> future = pool.schedule(() -> {
+                    int num = new Random().nextInt(100);
+                    System.out.println(Thread.currentThread().getName() + " : " + num);
+                    return num;
+            }, 2, TimeUnit.SECONDS);
+
+            System.out.println(future.get());
+        }
+
+        pool.shutdown();
+    }
+}
