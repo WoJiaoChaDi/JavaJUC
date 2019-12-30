87a425cc9fd3ef9373eb5585af23d593afa342b7
wojiaochadi <264835229@qq.com>
2019-12-30 15:40:34 +0800

    读写锁： 同一时刻允许多个线程共享资源进行读操作。 同一时刻只允许一个线程对共享资源进行写操作。 当进行写操作时，同一时刻其他线程的读操作会被阻塞。 当进行读操作是，同一时刻所有线程的写操作会被阻塞。

--- src/com/atguigu/juc/TestReadWriteLock.java
+++ src/com/atguigu/juc/TestReadWriteLock.java
@@ -1,5 +1,64 @@
 package com.atguigu.juc;
 
-public class TestReadWriteLock {
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 
+/*
+* 1. ReadWriteLock：读写锁
+*
+* 写写/读写 ：需要“互斥”
+* 读读 ：不需要“互斥”*/
+public class TestReadWriteLock {
+    public static void main(String[] args) {
+        ReadWriteLockDemo rw = new ReadWriteLockDemo();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                rw.set((int) (Math.random() * 101));
+            }
+        }, "Write").start();
+
+        for (int i = 0; i < 100; i++) {
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    rw.get();
+                }
+            }).start();
+
+        }
+    }
+}
+
+class ReadWriteLockDemo {
+    private int number = 0;
+
+    private ReadWriteLock lock = new ReentrantReadWriteLock();
+
+    //读
+    public void get() {
+        //上读锁
+        lock.readLock().lock();
+
+        try {
+            System.out.println(Thread.currentThread().getName() + " : " + number);
+        } finally {
+            //释放锁
+            lock.readLock().unlock();
+        }
+    }
+
+    //写
+    public void set(int number) {
+        //上写锁
+        lock.writeLock().lock();
+
+        try {
+            System.out.println(Thread.currentThread().getName());
+            this.number = number;
+        } finally {
+            //释放锁
+            lock.writeLock().unlock();
+        }
+    }
 }
