357a3a4dce877588c18187828c2393947200bead
wojiaochadi <264835229@qq.com>
2019-12-30 14:54:25 +0800

    this.wait() this.notifyAll() 添加等待唤醒机制
    添加等待唤醒机制后，如果用if else，可能存在某个线程无人唤醒的情况，所以需要去掉else，但是这个时候如果有多个生产者、消费者，则会在wait()后抢占资源，双双处于wait()状态，然后被一个消费者一起唤醒，一起进行消费，导致数据出错（虚假唤醒），所以wait()应该只用在while循环中，而不是if中，也就是说唤醒后需要再去判断一次

--- src/com/atguigu/juc/TestProductorAndConsumer.java
+++ src/com/atguigu/juc/TestProductorAndConsumer.java
@@ -19,7 +19,7 @@ class Clerk {
 
     //进货
     public synchronized void get(){
-        while (product >= 1) {
+        while (product >= 1) {//为了避免虚假唤醒问题，wait()应该总是使用在循环中才行
             System.out.println("产品已满！");
 
             //线程等待
