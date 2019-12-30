709ff3c7c33deda981a6e8796723507d8a572924
wojiaochadi <264835229@qq.com>
2019-12-30 12:37:13 +0800

    volatile无法保证原子性问题 使用原子性Atomic来保证原子性

--- src/com/atguigu/juc/TestAtomicDemo.java
+++ src/com/atguigu/juc/TestAtomicDemo.java
@@ -1,5 +1,7 @@
 package com.atguigu.juc;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class TestAtomicDemo {
 
     /*
@@ -11,6 +13,15 @@ public class TestAtomicDemo {
     *       i = i + 1;
     *       i = temp;
     *
+    * 2.原子变量：jdk1.5后，java.util.concurrent.atomic 包下提供了常用的原子变量
+    *   1. volatile  里面封装的变量都是volatile，保证内存可见性
+    *   2. CAS(CompareAndSwap)算法，保证数据的原子性
+    *           CAS 算法是硬件对于并发操作共享数据的支持
+    *           CAS 包含了三个操作数：
+    *           内存值 V
+    *           预估值 A
+    *           更新值 B
+    *           当且仅当 V == A 时， V = B，否则将不做任何操作
     *
     * */
     public static void main(String[] args) {
@@ -27,7 +38,8 @@ class AtomicDemo implements Runnable{
     //对于原子性问题,volatile也无能为力
     //因为线程里面对serialNumber有读-写操作，在一个线程写之前，另一个线程读取了旧数据，则两个线程写的时候会互相覆盖，导致无法保持原子性，及时他们都在主存中操作，也会有先后写覆盖的问题
     //private volatile int serialNumber = 0;
-    private int serialNumber = 0;
+    //private int serialNumber = 0;
+    private AtomicInteger serialNumber = new AtomicInteger(0);
 
     @Override
     public void run() {
@@ -41,6 +53,8 @@ class AtomicDemo implements Runnable{
     }
 
     public int getSerialNumber(){
-        return serialNumber++;
+        //return serialNumber++;
+        //return serialNumber.incrementAndGet();//++i
+        return serialNumber.getAndIncrement();//i++
     }
 }
